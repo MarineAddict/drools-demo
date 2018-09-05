3134b46be5bd3c9594aaa07f16720487b271e00f
xuqie <rick.xu@oyohotels.com>
2018-09-05 20:49:01 +0800

    added dentity

---
+++ src/main/java/com/oyo/droolsdemo/entity/model/Customer.java
@@ -0,0 +1,22 @@
+package com.oyo.droolsdemo.entity.model;
+
+import lombok.Data;
+
+/**
+ * @author: create by xuqie
+ * @description:
+ * @date:2018/9/5
+ */
+@Data
+public class Customer {
+
+    private String name;
+
+    private Integer age;
+
+    private boolean vip;
+
+
+
+
+}
